f2c567c2f85d086a3a519755fa42ca0a17fd68e2
Mtk Fujiu.jp <mtk-f@live.jp>
2016-03-07 17:58:25 +0900

    クソコード修正

--- TweetNicoCount/Program.cs
+++ TweetNicoCount/Program.cs
@@ -11,37 +11,9 @@ namespace TweetNicoCount
 {
     class Program
     {
-        private const string TWITTER_SCREEN_NAME = "@mtk_f";
-        private const string KEYWORD = "mmd クエリちゃん";
-        private const string TWEET_TAG = "#クエリちゃんの動画投稿数をツイートするサービス #自動";
-
-        // 非同期待ちフラグ.
-        private static bool IsDoing { get; set; } = false;
-
         static void Main()
         {
-            IsDoing = true;
-
-#pragma warning disable 4014
-            TweetQuerychanCountAsync();
-#pragma warning restore 4014
-
-            // エントリーポイントはasyncできないので
-            // 非同期処理が終わるまで待つ苦肉の策.
-            // wait async.
-            var startTime = DateTime.Now;
-            while (IsDoing)
-            {
-                Thread.Sleep(10);
-
-                // 無限ループ防止のため1分経ったら強制終了する.
-                var diff = DateTime.Now - startTime;
-                if (diff.TotalMinutes > 1)
-                {
-                    Console.WriteLine("timeout");
-                    break;
-                }
-            }
+            TweetQuerychanCountAsync().Wait();
 #if DEBUG
             Console.WriteLine("fin");
             Console.ReadLine();
@@ -50,16 +22,18 @@ namespace TweetNicoCount
 
         private static async Task TweetQuerychanCountAsync()
         {
+            const string TWITTER_SCREEN_NAME = "@mtk_f";
+            const string TWEET_TAG = "#クエリちゃんの動画投稿数をツイートするサービス #自動";
             const string URL = "http://api.search.nicovideo.jp/api/";
+            const string QUERY = "mmd クエリちゃん";
             const string MEDIA_TYPE = "application/json";
             var postConstnt
-                    = string.Format("{{\"query\":\"{0}\",\"service\":[\"video\"],\"search\":[\"title\",\"description\",\"tags\"],\"join\":[\"cmsid\",\"title\",\"description\",\"thumbnail_url\",\"start_time\",\"view_counter\",\"comment_counter\",\"mylist_counter\",\"channel_id\",\"main_community_id\",\"length_seconds\",\"last_res_body\"],\"filters\":[],\"sort_by\":\"start_time\",\"order\":\"desc\",\"from\":0,\"size\":25,\"timeout\":10000,\"issuer\":\"pc\",\"reason\":\"user\"}}", KEYWORD);
+                    = string.Format("{{\"query\":\"{0}\",\"service\":[\"video\"],\"search\":[\"title\",\"description\",\"tags\"],\"join\":[\"cmsid\",\"title\",\"description\",\"thumbnail_url\",\"start_time\",\"view_counter\",\"comment_counter\",\"mylist_counter\",\"channel_id\",\"main_community_id\",\"length_seconds\",\"last_res_body\"],\"filters\":[],\"sort_by\":\"start_time\",\"order\":\"desc\",\"from\":0,\"size\":25,\"timeout\":10000,\"issuer\":\"pc\",\"reason\":\"user\"}}", QUERY);
 
+            // nicovideo.jpをクエリー検索して結果のJSONを取得する.
             string rawJson = null;
-
             using (var client = new HttpClient())
             {
-                // 検索して結果のJSONを取得する.
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MEDIA_TYPE));
                 var content = new StringContent(postConstnt, System.Text.Encoding.UTF8, MEDIA_TYPE);
                 var response = await client.PostAsync(URL, content);
@@ -99,7 +73,7 @@ namespace TweetNicoCount
                     var msg = string.Format("nicovide.jpに投稿された {0} の動画は ＼ {1}件／ です\n"
                         + "http://search.nicovideo.jp/video/search/mmd%20%E3%82%AF%E3%82%A8%E3%83%AA%E3%81%A1%E3%82%83%E3%82%93?sort=upload_time\n"
                         + "{2}",
-                        KEYWORD, total, TWEET_TAG);
+                        QUERY, total, TWEET_TAG);
                     TweetMessage(msg);
                 }
                 catch (JsonException e)
@@ -111,12 +85,10 @@ namespace TweetNicoCount
             }
             else
             {
-                var msg = string.Format("{0} failed to get web contents {1}", TWITTER_SCREEN_NAME, TWEET_TAG);
+                var msg = string.Format("{0} failed to get Web API responses {1}", TWITTER_SCREEN_NAME, TWEET_TAG);
                 Console.WriteLine(msg);
                 TweetMessage(msg);
             }
-
-            IsDoing = false;
         }
 
         private static void TweetMessage(string msg)
